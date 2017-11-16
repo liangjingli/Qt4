94c85c8215d86b4b1f2f8b44d0cd5f12f9614c8d
liangjingli <jingli_liang@163.com>
2017-11-16 15:19:24 +0800

    MainWindow
    Spreadsheet

---
+++ MainWindow/mainwindow.cpp
@@ -0,0 +1,29 @@
+#include <QtGui>
+#include "finddialog.h"
+#include "gotocelldialog.h"
+#include "mainwindow.h"
+#include "sortdialog.h"
+#include "spreadsheet.h"
+
+MainWindow::MainWindow()
+	{
+		spreadsheet=new Spreadsheet();
+		setCentralWidget(spreadsheet);
+		
+		createActions();
+		createMenus();
+		createContextMenu();
+		createToolBars();
+		createStatusBar();
+		
+		readSettings();
+		
+		findDialog=0;
+		//setWindowIcon(QIcon(":/images/icon.png"));
+		setCurrentFile("");
+		}
+		
+void MainWindow::createActions()
+	{
+		
+		}
